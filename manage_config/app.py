77be1aa0f37cc014844628bbf5d9ff701c01cffc
RafalTaraska <paulo.dorosz@gmail.com>
2023-01-26 00:51:24 +0100

    v1.5

--- manage_config/app.py
+++ manage_config/app.py
@@ -6,19 +6,20 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def mainpage():
     data = request.args
-    if data:
-        ip = data.get('ip')
-        username = data.get('username')
-        password = data.get('password')
-        path = config.download_config(ip, username, password)
-        parameters = config.read_ini(path)
-        return render_template("config_details.html",parameters=parameters)
-    return render_template("mainpage.html")
-
-# @app.route("/details/<path>")
-# def config_details(path):
-#     parameters = config.read_ini(path)
-#     return render_template("config_details.html", parameters = parameters)    
+    if not data:
+        return render_template("mainpage.html")
+    ip = data.get('ip')
+    username = data.get('username')
+    password = data.get('password')
+    path = config.download_config(ip, username, password)
+    sections = config.read_ini(path)
+    return render_template("config_details.html",sections=sections)
+    
+@app.route("/parameter_details/<section>")
+def parameter_details(section):
+    parameter_details = config.read_parameter_details(section)
+    return render_template("parameter_details.html")
+  
 
 if __name__ == '__main__':
     app.run(debug=True)
--- manage_config/lib/endpoints.py
+++ manage_config/lib/endpoints.py
@@ -6,38 +6,55 @@ import base64
 import configparser
 
 class Config:
-    def __init__(self) -> None:
+    def __init__(self):
         pass
 
-    def download_config(self, ip, username, password):
-        folderpath = 'C:\\project\\audc_config_rest_api\\backups\\'
+    def ensure_path(self, folderpath):   
         if os.path.exists(folderpath) is not True:
             try:
                 os.mkdir(folderpath)
             except OSError:
-                print("Creation file filed" %folderpath)
+                print(f"Creation file filed {folderpath}")
             else:
-                print("File created" %folderpath)
+                print(f"File created {folderpath}")
+        return folderpath
+
+    def make_filepath(self, ip, folderpath):
         dt = str(datetime.datetime.now())
-        FilePath = folderpath + ip + "_" + dt[:10] + ".ini"
+        filepath = folderpath + ip + "_" + dt[:10] + ".ini"
+        return filepath
+    
+    def get_url_ini_file(self, ip):
         url = f'http://{ip}/api/v1/files/ini'
+        return url
+
+    def credential(self, username, password, url):
         cred = username + ':' + password
         cred_encoded = base64.b64encode(cred.encode()).decode()
-        headers = {'Authorization': 'Basic ' + cred_encoded}
-        response = requests.get(url, headers=headers)
-        response.status_code
-        if (response.status_code) == 200:
-            File = open(FilePath,"w")
-            File.write(response.text)
-            File.close()
-        else:
-            print("Backup filed: ", response.status_code) 
-        return FilePath
+        headers = {'Authorization': 'Basic ' + cred_encoded} 
+        return requests.get(url, headers=headers)
 
-    def read_ini(self, file_name): 
+    def download_config(self, ip, username, password):
+        folderpath = self.ensure_path("./backups/")        
+        filepath = self.make_filepath(ip, folderpath)
+        url = self.get_url_ini_file(ip)
+        response = self.credential(username, password, url)
+        if response.status_code != 200:
+            print("Backup filed: ", response.status_code)
+            return None
+        with open(filepath, "w") as f:
+            f.write(response.text)
+        return filepath
+
+    def read_ini(self, path): 
         config = configparser.ConfigParser()
         config.sections()
-        config.read(file_name)
+        config.read(path)
         return config.sections()
+
+    def read_parameter_details(self, parameter):
+        for key, value in config[parameter].items():
+            return key, value
+
 
 config  = Config()
